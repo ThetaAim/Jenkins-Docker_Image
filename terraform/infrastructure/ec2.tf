ca7e3a23316d8ebabb71a444cc242166309ffa1e
ThetaAim <mailorenn@gmail.com>
2026-03-17 22:14:01 +0200

    temp save

--- terraform/infrastructure/ec2.tf
+++ terraform/infrastructure/ec2.tf
@@ -3,9 +3,12 @@ resource "aws_instance" "ec2_micro_ecr" {
 
   ami                         = "ami-0e3ce61b0fbdbe6ee" # Self made Image with Packer
   instance_type               = "t3.micro"
-  subnet_id                   = element([aws_subnet.private_a.id, aws_subnet.private_b.id], count.index)
+  
+  subnet_id                   = aws_subnet.public_a.id
+  # subnet_id                   = element([aws_subnet.private_a.id, aws_subnet.private_b.id], count.index)
   vpc_security_group_ids      = [aws_security_group.ec2_private_sg.id]
-  associate_public_ip_address = false
+  associate_public_ip_address = true
+  # associate_public_ip_address = false
   key_name                    = "jenkins"
   iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
 
